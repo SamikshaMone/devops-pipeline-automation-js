f88596deec56b7ff209e6bb52e96c6d2dc4b1656
Samiksha Mone <samikshamone6@gmail.com>
2024-08-04 11:43:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Middleware setup
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.post('/data', (req, res) => {
+    // Handle post request
+    res.send('Data received');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
